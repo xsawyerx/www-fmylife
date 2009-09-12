2c3b412410f2d5dea69204c2f0693432fd603882
Sawyer X <xsawyerx@cpan.org>
2009-09-13 00:12:20 +0300

    upgrading version

--- lib/WWW/FMyLife.pm
+++ lib/WWW/FMyLife.pm
@@ -5,7 +5,7 @@ use XML::Simple;
 use LWP::UserAgent;
 use WWW::FMyLife::Item;
 
-our $VERSION = '0.06';
+our $VERSION = '0.07';
 
 has 'username' => ( is => 'rw', isa => 'Str' );
 has 'password' => ( is => 'rw', isa => 'Str' );
@@ -242,7 +242,7 @@ WWW::FMyLife - Obtain FMyLife.com anecdotes via API
 
 =head1 VERSION
 
-Version 0.06
+Version 0.07
 
 =head1 SYNOPSIS
 
