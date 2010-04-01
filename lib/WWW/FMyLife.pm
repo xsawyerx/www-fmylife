0621df4235f0c66edffd9d0cb6d6698f3d90943a
Sawyer X <xsawyerx@cpan.org>
2010-04-01 11:40:08 +0300

    releasing 0.15

--- lib/WWW/FMyLife.pm
+++ lib/WWW/FMyLife.pm
@@ -5,7 +5,7 @@ use XML::Simple;
 use LWP::UserAgent;
 use WWW::FMyLife::Item;
 
-our $VERSION = '0.14';
+our $VERSION = '0.15';
 
 has 'username' => ( is => 'rw', isa => 'Str' );
 has 'password' => ( is => 'rw', isa => 'Str' );
@@ -260,7 +260,7 @@ WWW::FMyLife - Obtain FMyLife.com anecdotes via API
 
 =head1 VERSION
 
-Version 0.14
+Version 0.15
 
 =head1 SYNOPSIS
 
